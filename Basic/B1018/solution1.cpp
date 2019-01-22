bcd06b2e659f1e3d82019eddab950faa32c67898
hhlb <hhlb1997@outlook.com>
2019-01-22 19:27:03 +0800

    fix bug B1018

--- Basic/B1018/solution1.cpp
+++ Basic/B1018/solution1.cpp
@@ -1,12 +1,11 @@
 #include <cstdio>
 
+int change(char c) {
+  if (c == 'B') return 0;
+  if (c == 'C') return 1;
+  if (c == 'J') return 2;
+}
 int main() {
-  int change(char c) {
-    if (c == "B") return 0;
-    if (c == "C") return 1;
-    if (c == "J") return 2;
-  }
-
   int n;
   int times_a[3] = {0}, times_b[3] = {0};
   int hand_a[3] = {0}, hand_b[3] = {0};
@@ -16,21 +15,21 @@ int main() {
   int k1, k2;
   for (int i = 0; i < n; ++i) {
     getchar();
-    scanf("%c%c", &c1, &c2);
+    scanf("%c %c", &c1, &c2);
     k1 = change(c1);
     k2 = change(c2);
-    if ((k + 1) % 3 == k2) {
+    if ((k1 + 1) % 3 == k2) {
       ++times_a[0];
       ++times_b[2];
       ++hand_a[k1];
-    } else if (k1 == k2)
+    } else if (k1 == k2) {
       ++times_a[1];
-    ++times_b[1];
-  }
-  else if ((k2 + 1) % 3 == k1) {
-    ++times_a[2];
-    ++times_a[0];
-    ++hand_b[k2];
+      ++times_b[1];
+    } else {
+      ++times_a[2];
+      ++times_b[0];
+      ++hand_b[k2];
+    }
   }
   printf("%d %d %d\n", times_a[0], times_a[1], times_a[2]);
   printf("%d %d %d\n", times_b[0], times_b[1], times_b[2]);
