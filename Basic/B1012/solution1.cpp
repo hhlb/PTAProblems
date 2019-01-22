f8d1503bfd6e5c0945a90e84a698f3b9b55ac805
hhlb <hhlb1997@outlook.com>
2019-01-22 19:41:00 +0800

    fix bug B1012

--- Basic/B1012/solution1.cpp
+++ Basic/B1012/solution1.cpp
@@ -3,32 +3,49 @@
 int main() {
   int n;
   int count[5] = {0};
-  int a2 = 0, a4 = 0, a5_max = 0;
+  int a2 = 0, a4 = 0;
   scanf("%d", &n);
   for (int i = 0; i < n; ++i) {
     int number;
     scanf("%d", &number);
-    if (number % 10 == 0) {
-      count[0] += number;
+    if (number % 5 == 0) {
+      if (number % 2 == 0) count[0] += number;
     } else if (number % 5 == 1) {
-      count[1] += number * (-1) ^ a2;
+      if (a2 % 2 == 0) 
+        count[1] += number;
+      else
+        count[1] -= number;
       ++a2;
     } else if (number % 5 == 2) {
-      ++count[2]
+      ++count[2];
     } else if (number % 5 == 3) {
       count[3] += number;
-      ++a4
+      ++a4;
     } else {
-      if (number > a5_max) {
-        a5_max = number;
+      if (number > count[4]) {
+        count[4] = number;
       }
     }
   }
-  count[3] /= a4;
-  count[4] = a5_max;
-  for (int i = 0; i < n; ++i) {
-    if (count[i] == 0) printf("N");
-    if (i != n - 1) printf(" ");
-  }
+  if (count[0] == 0)
+    printf("N ");
+  else
+    printf("%d ", count[0]);
+  if (count[1] == 0)
+    printf("N ");
+  else
+    printf("%d ", count[1]);
+  if (count[2] == 0)
+    printf("N ");
+  else
+    printf("%d ", count[2]);
+  if (count[3] == 0)
+    printf("N ");
+  else
+    printf("%.1f ", (double)count[3] / a4);
+  if (count[4] == 0)
+    printf("N");
+  else
+    printf("%d", count[4]);
   return 0;
 }
