55c73c5fc6f274c0639c813ce06123b6ca33584e
hhlb <hhlb1997@outlook.com>
2019-01-22 12:03:44 +0800

    add basic B1018

---
+++ Basic/B1018/solution1.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+
+int main() {
+  int change(char c) {
+    if (c == "B") return 0;
+    if (c == "C") return 1;
+    if (c == "J") return 2;
+  }
+
+  int n;
+  int times_a[3] = {0}, times_b[3] = {0};
+  int hand_a[3] = {0}, hand_b[3] = {0};
+  char mp[3] = {'B', 'C', 'J'};
+  scanf("%d", &n);
+  char c1, c2;
+  int k1, k2;
+  for (int i = 0; i < n; ++i) {
+    getchar();
+    scanf("%c%c", &c1, &c2);
+    k1 = change(c1);
+    k2 = change(c2);
+    if ((k + 1) % 3 == k2) {
+      ++times_a[0];
+      ++times_b[2];
+      ++hand_a[k1];
+    } else if (k1 == k2)
+      ++times_a[1];
+    ++times_b[1];
+  }
+  else if ((k2 + 1) % 3 == k1) {
+    ++times_a[2];
+    ++times_a[0];
+    ++hand_b[k2];
+  }
+  printf("%d %d %d\n", times_a[0], times_a[1], times_a[2]);
+  printf("%d %d %d\n", times_b[0], times_b[1], times_b[2]);
+  int id1 = 0, id2 = 0;
+  for (int i = 0; i < 3; ++i) {
+    if (hand_a[i] > hand_a[id1]) id1 = i;
+    if (hand_b[i] > hand_b[id2]) id2 = i;
+  }
+  printf("%c %c", mp[id1], mp[id2]);
+  return 0;
+}
