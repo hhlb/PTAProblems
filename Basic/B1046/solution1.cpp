535753ff141dcb4b152616147fbd7d2703b5622f
hhlb <hhlb1997@outlook.com>
2019-01-22 19:27:16 +0800

    fix bug B1046

--- Basic/B1046/solution1.cpp
+++ Basic/B1046/solution1.cpp
@@ -4,7 +4,7 @@ int main() {
   int t;
   int a = 0, b = 0;
   scanf("%d", &t);
-  while (--t) {
+  while (t--) {
     int a0, a1, b0, b1;
     scanf("%d%d%d%d", &a0, &a1, &b0, &b1);
     if (a0 + b0 == a1 && a0 + b0 != b1) ++b;
