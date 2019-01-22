c556d3aa6ffefb8e0778436d39adde1dff1cbb14
hhlb <hhlb1997@outlook.com>
2019-01-22 12:03:53 +0800

    add basic B1026

---
+++ Basic/B1026/solution1.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+  int a, b, t;
+  scanf("%d%d", &a, &b);
+  t = b - a;
+  if (t % 100 >= 50) {
+    t = t / 100 + 1;
+
+  } else {
+    t /= 100;
+  }
+  printf("%02d:%02d:%02d", t / 3600, t % 3600 / 60, t % 60);
+  return 0;
+}
