ed0dcacb3048a4642f01451e1757b52cebbdc0a2
hhlb <hhlb1997@outlook.com>
2019-01-24 20:45:49 +0800

    add basic B1036

---
+++ Basic/B1036/solution1.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+
+int main() {
+  int c;
+  char b;
+  scanf("%d %c", &c, &b);
+  int a = c;
+  a *= 5;
+  if (a % 10 >= 5)
+    a = a / 10 + 1;
+  else
+    a /= 10;
+  for (int i = 0; i < c; ++i) {
+    printf("%c", b);
+  }
+  printf("\n");
+  for (int i = 0; i < a - 2; ++i) {
+    printf("%c", b);
+    for (int j = 0; j < c - 2; ++j) {
+      printf(" ");
+    }
+    printf("%c\n", b);
+  }
+  // for (int i = 0; i < (a - 2) * 2 + 1; ++i) {
+  //   if (i % 2 == 0) {
+  //     for (int j = 0; j < c; ++j) {
+  //       printf(" ");
+  //     }
+  //     printf("\n");
+  //   } else {
+  //     printf("%c", b);
+  //     for (int j = 0; j < c - 2; ++j) {
+  //       printf(" ");
+  //     }
+  //     printf("%c\n", b);
+  //   }
+  // }
+  for (int i = 0; i < c; ++i) {
+    printf("%c", b);
+  }
+  return 0;
+}
