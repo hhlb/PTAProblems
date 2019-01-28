9687d852f4b5d1c1f71d2e3720c29175d3f14e1e
hhlb <hhlb1997@outlook.com>
2019-01-28 20:08:17 +0800

    change the way to init the array

--- Basic/B1002/solution1.cpp
+++ Basic/B1002/solution1.cpp
@@ -2,17 +2,8 @@
 #include <cstdio>
 int main() {
   char s[100];
-  char* c[10];
-  c[0] = "ling";
-  c[1] = "yi";
-  c[2] = "er";
-  c[3] = "san";
-  c[4] = "si";
-  c[5] = "wu";
-  c[6] = "liu";
-  c[7] = "qi";
-  c[8] = "ba";
-  c[9] = "jiu";
+  char* c[10] = {"ling", "yi",  "er", "san", "si",
+                 "wu",   "liu", "qi", "ba",  "jiu"};
   int sum = 0;
   int l = 0;
   while ((s[l] = getchar()) != '\n') {
@@ -26,9 +17,9 @@ int main() {
     sum /= 10;
     ++i;
   } while (sum != 0);
-  while(i--){
+  while (i--) {
     printf("%s", c[a[i]]);
-    if(i!=0) printf(" ");
+    if (i != 0) printf(" ");
   }
   return 0;
 }
